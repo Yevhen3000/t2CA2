f43f713dd724a460ca7701965ed53380bedf374e
Yevhen K @CCT <sba23066@student.cct.ie>
2024-02-19 19:41:07 +0000

    Init

---
+++ src/main/java/com/mycompany/t2ca2/T2CA2.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.t2ca2;
+
+/**
+ * @date start    19/02/24
+ * @author  Yevhen Kuropiatnyk
+ * @email   evgeniy.kuropyatnik@gmail.com
+ * @student sba23066
+ */
+public class T2CA2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
